916a2cc4f3f36bbdf93774a8bbb0332cc9da04ed
Armia Joseph <armia.joseph35@gmail.com>
2024-12-25 14:36:32 +0200

    feat: changed signature of token constructor

--- src/main/java/org/example/rakkenishokran/Entities/Token.java
+++ src/main/java/org/example/rakkenishokran/Entities/Token.java
@@ -1,8 +1,10 @@
 package org.example.rakkenishokran.Entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Token {
     int id;
     String token;
