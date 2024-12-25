d9b79638292396de01cda56649d139f1a156eed7
Armia Joseph <armia.joseph35@gmail.com>
2024-12-25 14:36:07 +0200

    feat: implemented user repo with jdbc

--- src/main/java/org/example/rakkenishokran/Repositories/UserRepository.java
+++ src/main/java/org/example/rakkenishokran/Repositories/UserRepository.java
@@ -2,10 +2,12 @@ package org.example.rakkenishokran.Repositories;
 
 import org.example.rakkenishokran.Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRepository {
@@ -14,7 +16,7 @@ public class UserRepository {
     private JdbcTemplate jdbcTemplate;
 
     public List<User> findAll() {
-        return jdbcTemplate.query("SELECT * FROM users", (rs, rowNum) ->
+        return jdbcTemplate.query("SELECT * FROM USER", (rs, rowNum) ->
                 new User(
                         rs.getLong("id"),
                         rs.getString("email"),
@@ -24,8 +26,62 @@ public class UserRepository {
                 ));
     }
 
-    public void save(User user) {
-        jdbcTemplate.update("INSERT INTO users (name, email) VALUES (?, ?)",
-                user.getName(), user.getEmail());
+    public Optional<User> findByEmail(String email) {
+        List<User> users = jdbcTemplate.query(
+                "SELECT * FROM USER WHERE email = ?",
+                (rs, rowNum) -> new User(
+                        rs.getLong("id"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("phoneNumber")
+                ),
+                email
+        );
+        if (users.isEmpty())
+            throw new EmptyResultDataAccessException(1);
+        else
+            return Optional.of(users.get(0));
     }
+
+    public Optional<User> findByUsername(String username) {
+        List<User> users = jdbcTemplate.query(
+                "SELECT * FROM USER WHERE name = ?",
+                (rs, rowNum) -> new User(
+                        rs.getLong("id"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("phoneNumber")
+                ),
+                username
+        );
+        if (users.isEmpty())
+            throw new EmptyResultDataAccessException(1);
+        else
+            return Optional.of(users.get(0));
+    }
+
+    public void save(User user) {
+        jdbcTemplate.update("INSERT INTO USER (name, phoneNumber, email, password) VALUES (?, ?, ?, ?)",
+                user.getName(),user.getPhoneNumber(), user.getEmail(), user.getPassword());
+    }
+
+    public void update(User user) {
+        jdbcTemplate.update("UPDATE USER SET name = ?, phoneNumber = ?, email = ?, password = ? WHERE id = ?",
+                user.getName(), user.getPhoneNumber(), user.getEmail(), user.getPassword(), user.getId());
+    }
+
+    public void deleteById(User user) {
+        jdbcTemplate.update("DELETE FROM USER WHERE id = ?", user.getId());
+    }
+
+    public void deleteByEmail(User user) {
+        jdbcTemplate.update("DELETE FROM USER WHERE email = ?", user.getEmail());
+    }
+
+    public void deleteByUsername(User user) {
+        jdbcTemplate.update("DELETE FROM USER WHERE name = ?", user.getName());
+    }
+
 }
