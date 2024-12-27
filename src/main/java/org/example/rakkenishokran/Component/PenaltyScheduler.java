f09c6bfb6a7afc6105530d9150c4a41f42ba80bc
PierreJack <pierrejacques2002@gmail.com>
2024-12-27 18:49:25 +0200

    Update PenaltyScheduler.java

--- src/main/java/org/example/rakkenishokran/Component/PenaltyScheduler.java
+++ src/main/java/org/example/rakkenishokran/Component/PenaltyScheduler.java
@@ -13,13 +13,19 @@ public class PenaltyScheduler {
     @Autowired
     private DataSource dataSource;
 
-    @Scheduled(fixedRate = 30000) // Runs every 30 seconds
+    @Scheduled(fixedRate = 3000) // Runs every 30 seconds
     public void applyPenalties() {
+
         String query = "INSERT INTO PENALTY (fees, type, userId) " +
                 "SELECT 50 AS fees, 'NotShowingUp' AS type, R.userId " +
                 "FROM RESERVATION R " +
                 "LEFT JOIN LOG_TABLE L ON R.id = L.reservationId " +
-                "WHERE R.endTimeStamp < CURRENT_TIMESTAMP AND L.reservationId IS NULL;";
+                "WHERE R.endTimeStamp < CURRENT_TIMESTAMP " +
+                "AND L.reservationId IS NULL " +
+                "AND NOT EXISTS ( " +
+                "    SELECT 1 FROM PENALTY P " +
+                "    WHERE P.userId = R.userId AND P.type = 'NotShowingUp'" +
+                ");";
 
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
